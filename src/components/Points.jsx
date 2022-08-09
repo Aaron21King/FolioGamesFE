1bc9f1186e0b11e82f3fd76b7b64c1ecf3681903
noe-quezada <noequesada@folio3.com>
2022-08-09 14:25:39 -0500

    somethin weird happened

---
+++ src/components/Points.jsx
@@ -0,0 +1,47 @@
+function Points(params) {
+  return (
+    <div className="bg-image">
+      <img src="images/logo.png" class="img-fluid" alt="..."></img>
+      <table class="table table-dark table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>
+              <button type="button" class="btn btn-primary">
+                +3
+              </button>
+              <button type="button" class="btn btn-primary">
+                +5
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Points;
