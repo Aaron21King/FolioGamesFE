c0252209903932af9b8686d9055a44218da93334
Aaron21King <lovespurs1882@gmail.com>
2022-08-10 11:19:56 -0600

    Ventana players

--- src/components/Players/index.jsx
+++ src/components/Players/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BsFillTrashFill } from "react-icons/bs";
+import '../Players/players.css';
 
 function Players(params) {
   const [get, setGet] = useState([]);
@@ -17,8 +18,12 @@ function Players(params) {
   });
   return (
     <div className="bg-image">
-      <img src="images/logo.png" className="img-fluid" alt="..."></img>
-
+      <img src="images/logo.png"  className="img-fluid" alt="..."></img>
+      <div className="submit">
+      <input type="text" name="usuario"></input>
+      <input type="submit" value="Enviar datos"></input>
+      </div>
+        <hr></hr>
       <table className="table table-dark table-striped">
         <thead>
           <tr>
@@ -36,19 +41,6 @@ function Players(params) {
               </tr>
             );
           })}
-          {/* <tr>
-          <th scope="row">1 </th>
-          <td>{name}</td>
-          <td>{points}</td>
-          <td>
-            <button type="button" className="btn btn-primary">
-              +3
-            </button>
-            <button type="button" className="btn btn-primary">
-              +5
-            </button>
-          </td>
-        </tr> */}
         </tbody>
       </table>
     </div>
