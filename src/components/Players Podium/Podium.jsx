7837fa4da5a0d2f78eca96f1eaf5ee7b72f88223
noe-quezada <noequesada@folio3.com>
2022-08-11 12:17:41 -0500

    Added and finished the functionality of podium page

--- src/components/Players Podium/Podium.jsx
+++ src/components/Players Podium/Podium.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Podium() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/players")
+      .then((res) => {
+        console.log(res);
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  posts.sort((a, b) => {
+    return b.points - a.points;
+  });
+
   return (
     <div className="podium">
       <img
@@ -11,15 +30,42 @@ function Podium() {
       <section className="podium-medals">
         <div className="second">
           <img src="images/silver.png" alt="" className="img-medals" />
-          <h1>Noe</h1>
+          <div>
+            {posts.slice(1, 2).map((post, index) => {
+              return (
+                <div key={index}>
+                  <h1>{post.name}</h1>
+                  <h1>{post.points}</h1>
+                </div>
+              );
+            })}
+          </div>
         </div>
         <div className="first">
           <img src="images/gold.png" alt="" className="img-medals-first" />
-          <h1>Javier</h1>
+          <div>
+            {posts.slice(0, 1).map((post, index) => {
+              return (
+                <div key={index}>
+                  <h1>{post.name}</h1>
+                  <h1>{post.points}</h1>
+                </div>
+              );
+            })}
+          </div>
         </div>
         <div className="third">
           <img src="images/bronze.png" alt="" className="img-medals" />
-          <h1>Aaron</h1>
+          <div>
+            {posts.slice(2, 3).map((post, index) => {
+              return (
+                <div key={index}>
+                  <h1>{post.name}</h1>
+                  <h1>{post.points}</h1>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
     </div>
