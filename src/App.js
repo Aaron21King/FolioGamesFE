1bf40bd967a83da3ddad9b81553adba1afd49c8a
noe-quezada <noequesada@folio3.com>
2022-08-11 13:48:15 -0500

    added the changes from Rodrigo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Points from "./components/Players Points/Points";
 import Players from "./components/Players/index";
-import Podium from "./components/Players Podium/Podium"
+import Podium from "./components/Players Podium/Podium";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/points" element={<Points />} />
-        <Route path="/players" element={<Players />} />
+        <Route path="/" element={<Players />} />
         <Route path="/podium" element={<Podium />} />
       </Routes>
     </div>
-    
   );
 }
 
--- src/components/Players/index.jsx
+++ src/components/Players/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {BrowserRouter as Router,Route,Routes,Link} from "react-router-dom";
-import '../Players/players.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "../Players/players.css";
 
 function Players(params) {
-  const[namePlayer]=React.useState({
-    name:''
-  });
+  let rowCounter = 1;
+  const [namePlayer, setNamePlayer] = React.useState("");
+  const [players, setPlayers] = useState([]);
+
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     axios
@@ -18,24 +19,49 @@ function Players(params) {
       .catch((err) => {
         console.log(err);
       });
+  }, []);
 
-      
-  },[]);
-  const addPost =  () =>{
-    const postName = {name:namePlayer, points:0};
-     axios.post("http://localhost:8000/api/players",postName).then((res)=>{console.log(res)}).catch((err)=>{
-      setPosts([postName, ...posts]);
-     })
+  const addPost = (e) => {
+    e.preventDefault();
+    setPlayers([...players, namePlayer]);
+    setNamePlayer("");
   };
-  
+
+  const handleChange = (e) => {
+    setNamePlayer(e.target.value);
+  };
+
+  const sendData = () => {
+    axios
+      .post("http://localhost:8000/api/players", players)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        setPosts([...posts]);
+      });
+  };
+
   return (
     <div className="bg-image">
-      <img src="images/logo.png"  className="img-fluid" alt="..."></img>
+      <img src="images/logo.png" className="img-fluid" alt="..."></img>
       <div className="submitUser">
-      <input style={{borderRadius: "7px",width:"17rem", height:"2.5rem"}}  type="text"  name="usuario"></input>
-      <input style={{borderRadius: "7px",width:"7rem", height:"2.5rem"}} type="submit" value="Agregar" ></input>
+        <input
+          style={{ borderRadius: "7px", width: "17rem", height: "2.5rem" }}
+          placeholder="Player Name"
+          type="text"
+          name="usuario"
+          onChange={handleChange}
+          value={namePlayer}
+        ></input>
+        <input
+          style={{ borderRadius: "7px", width: "7rem", height: "2.5rem" }}
+          type="submit"
+          value="Agregar"
+          onClick={addPost}
+        ></input>
       </div>
-        <hr></hr>
+      <hr></hr>
       <table className="table table-dark table-striped">
         <thead>
           <tr>
@@ -44,22 +70,24 @@ function Players(params) {
           </tr>
         </thead>
         <tbody>
-          {posts.map((post) => {
+          {players.map((post) => {
             return (
               <tr>
-                <th scope="row">{+1}</th>
-                <td>{post.name}</td>
+                <th scope="row">{rowCounter++}</th>
+                <td>{post}</td>
               </tr>
             );
           })}
         </tbody>
       </table>
-      <button
-      type="button"
-      className="btn btn-success">
-				<Link style={{color:"white",textDecorationLine:"none"}} to='/points'>Let the Games Begin!!⚾⚾</Link>
-		</button>
-     
+      <button onClick={sendData} type="button" className="btn btn-success">
+        <Link
+          style={{ color: "white", textDecorationLine: "none" }}
+          to="/points"
+        >
+          Let the Games Begin!!⚾⚾
+        </Link>
+      </button>
     </div>
   );
 }
