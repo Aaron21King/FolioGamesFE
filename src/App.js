d7644d9d0858ab62348200df9af00aa775cf65e0
Aaron21King <lovespurs1882@gmail.com>
2022-08-11 13:38:13 -0600

    Agregando error 404

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Routes, Route} from "react-router-dom";
 import Points from "./components/Players Points/Points";
 import Players from "./components/Players/index";
 import Podium from "./components/Players Podium/Podium";
+import Error404 from "./components/Error404/index";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/points" element={<Points />} />
-        <Route path="/" element={<Players />} />
-        <Route path="/podium" element={<Podium />} />
+        <Route exact path="/points" element={<Points />} />
+        <Route exact path="/" element={<Players />} />
+        <Route exact path="/podium" element={<Podium />} />
+        <Route path="*" element={<Error404/>}/>
       </Routes>
     </div>
   );
