01b6247277bb6536f81ca88e874cc7e5b8d85a93
Aaron21King <lovespurs1882@gmail.com>
2022-08-11 14:06:38 -0600

    Estilos error 404

--- src/components/Error404/index.jsx
+++ src/components/Error404/index.jsx
@@ -4,7 +4,7 @@ const Error404 = ()=>{
         <div className="Error404">
             <h1>Error 404</h1>
             <h3>Not Found</h3>
-            <p>The Page you are looking for doesn't exist or another error ocurred.</p>
+            <h5>The Page you are looking for doesn't exist or another error ocurred.</h5>
         </div>
     )
 }
