c48e20d3b5b7aac422bdbd51ae9cb024a68d49b9
Aaron21King <lovespurs1882@gmail.com>
2022-08-11 13:54:58 -0600

    Editando error 404

--- src/components/Error404/index.jsx
+++ src/components/Error404/index.jsx
@@ -1,7 +1,7 @@
-
+import "../Error404/error.css"
 const Error404 = ()=>{
     return(
-        <div>
+        <div className="Error404">
             <h1>Error 404</h1>
             <h3>Not Found</h3>
             <p>The Page you are looking for doesn't exist or another error ocurred.</p>
