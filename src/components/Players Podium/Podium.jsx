073861eb93a32c18de98f60f316c30dae525f0bb
noe-quezada <noequesada@folio3.com>
2022-08-11 16:18:29 -0500

    added loader and a button to go to principal on both Podium and Points views

--- src/components/Players Podium/Podium.jsx
+++ src/components/Players Podium/Podium.jsx
@@ -1,20 +1,75 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import LoadingSpinner from "../Loading-Spinner/Loading-Spinner";
+import { useNavigate } from "react-router-dom";
 
 function Podium() {
+  const [isLoading, setIsLoading] = useState(false);
   const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("http://localhost:8000/api/players")
       .then((res) => {
         console.log(res);
         setPosts(res.data);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   }, []);
+
+  const navigateToPrincipal = () => {
+    navigate("/");
+  };
+
+  const renderPodium = (
+    <section className="podium-medals">
+      <div className="second">
+        <img src="images/silver.png" alt="" className="img-medals" />
+        <div>
+          {posts.slice(1, 2).map((post, index) => {
+            return (
+              <div key={index}>
+                <h1>{post.name}</h1>
+                <h1>{post.points}</h1>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="first">
+        <img src="images/gold.png" alt="" className="img-medals-first" />
+        <div>
+          {posts.slice(0, 1).map((post, index) => {
+            return (
+              <div key={index}>
+                <h1>{post.name}</h1>
+                <h1>{post.points}</h1>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="third">
+        <img src="images/bronze.png" alt="" className="img-medals" />
+        <div>
+          {posts.slice(2, 3).map((post, index) => {
+            return (
+              <div key={index}>
+                <h1>{post.name}</h1>
+                <h1>{post.points}</h1>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 
   posts.sort((a, b) => {
     return b.points - a.points;
@@ -27,47 +82,14 @@ function Podium() {
         className="img-fluid2"
         alt="..."
       ></img>
-      <section className="podium-medals">
-        <div className="second">
-          <img src="images/silver.png" alt="" className="img-medals" />
-          <div>
-            {posts.slice(1, 2).map((post, index) => {
-              return (
-                <div key={index}>
-                  <h1>{post.name}</h1>
-                  <h1>{post.points}</h1>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className="first">
-          <img src="images/gold.png" alt="" className="img-medals-first" />
-          <div>
-            {posts.slice(0, 1).map((post, index) => {
-              return (
-                <div key={index}>
-                  <h1>{post.name}</h1>
-                  <h1>{post.points}</h1>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className="third">
-          <img src="images/bronze.png" alt="" className="img-medals" />
-          <div>
-            {posts.slice(2, 3).map((post, index) => {
-              return (
-                <div key={index}>
-                  <h1>{post.name}</h1>
-                  <h1>{post.points}</h1>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </section>
+      {isLoading ? <LoadingSpinner /> : renderPodium}
+      <button
+        type="button"
+        className="btn btn-success"
+        onClick={navigateToPrincipal}
+      >
+        Start a New Game
+      </button>
     </div>
   );
 }
--- src/components/Players Points/Points.jsx
+++ src/components/Players Points/Points.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BsFillTrashFill } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "../Loading-Spinner/Loading-Spinner";
 
 function Points() {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   let rowCounter = 1;
   const navigate = useNavigate();
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("http://localhost:8000/api/players")
       .then((res) => {
         console.log(res);
         setPosts(res.data);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
@@ -67,54 +71,72 @@ function Points() {
     navigate("/podium");
   };
 
+  const navigateToPrincipal = () => {
+    navigate("/");
+  };
+
+  const renderPoints = (
+    <table className="table table-dark table-striped">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Points</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {posts.map((post) => {
+          return (
+            <tr key={post._id}>
+              <th scope="row">{rowCounter++}</th>
+              <td>{post.name}</td>
+              <td>{post.points}</td>
+              <td>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => handleUpdate3(post)}
+                >
+                  +3
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => handleUpdate5(post)}
+                >
+                  +5
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(post)}
+                >
+                  <BsFillTrashFill />
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+
   return (
     <div className="bg-image">
+      <div className="goBack">
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={navigateToPrincipal}
+        >
+          Go back
+        </button>
+      </div>
       <img src="images/logo.png" className="img-fluid" alt="..."></img>
 
-      <table className="table table-dark table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Points</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {posts.map((post) => {
-            return (
-              <tr key={post._id}>
-                <th scope="row">{rowCounter++}</th>
-                <td>{post.name}</td>
-                <td>{post.points}</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={() => handleUpdate3(post)}
-                  >
-                    +3
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={() => handleUpdate5(post)}
-                  >
-                    +5
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-danger"
-                    onClick={() => handleDelete(post)}
-                  >
-                    <BsFillTrashFill />
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {isLoading ? <LoadingSpinner /> : renderPoints}
+
       <button
         type="button"
         className="btn btn-success"
